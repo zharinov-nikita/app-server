fa13b154a3a0976b1cc96a7e4505b1024fdc6e23
Nikita <zharinov.nicita@gmail.com>
2022-06-04 09:56:33 +0700

    Dist remove
    Удалил директорию dist в котрою собирался проект из TypeSript в JavaScript

--- dist/microservice/project/messages/message.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-class Message {
-    constructor() {
-        this.show = (level, content) => ({ message: { level, content } });
-    }
-}
-exports.default = new Message();
